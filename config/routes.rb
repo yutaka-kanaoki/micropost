8b776a2338802f0d76927bdf0db4350eb627d84e
yutaka.kanaoki <kgggg596@yahoo.co.jp>
2019-09-07 03:23:47 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
   
+  get 'microposts/create'
+  get 'microposts/destroy'
+  get 'create/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'toppages#index'
 
@@ -10,5 +13,6 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [ :index, :show, :new, :create ]
+  resources :microposts, only: [:create, :destroy]
   
 end
