39e088a14eef86032200b5ab2a38ad62a5ef98f5
yutaka.kanaoki <kgggg596@yahoo.co.jp>
2019-09-08 04:06:26 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,7 +4,7 @@ class MicropostsController < ApplicationController
   before_action :correct_user, only: [:destroy]
   
   def create
-    @micropost = current_user.microposts.build(micropost_params)
+    @micropost = current_user.feed_microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = 'メッセージを投稿しました'
       redirect_to root_url
